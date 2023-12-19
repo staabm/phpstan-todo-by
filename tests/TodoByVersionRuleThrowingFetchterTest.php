cf8a02dd801e5f59ba91c59089419b7f33308b11
Markus Staab <maggus.staab@googlemail.com>
2023-12-19 08:16:25 +0100

    Test laziness of TagFetcher

---
+++ tests/TodoByVersionRuleThrowingFetchterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace staabm\PHPStanTodoBy\Tests;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use staabm\PHPStanTodoBy\ReferenceVersionFinder;
+use staabm\PHPStanTodoBy\TodoByVersionRule;
+
+/**
+ * @extends RuleTestCase<TodoByVersionRule>
+ */
+final class TodoByVersionRuleThrowingFetchterTest extends RuleTestCase
+{
+    private string $referenceVersion;
+    protected function getRule(): Rule
+    {
+        return new TodoByVersionRule(true, new ReferenceVersionFinder($this->referenceVersion, new AlwaysThrowingTagFetcher()));
+    }
+
+    public function testRule(): void
+    {
+        $this->referenceVersion = "0.1";
+
+        $this->analyse([__DIR__ . '/data/regularComments.php'], []);
+    }
+
+}
