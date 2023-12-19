2ab5d82ac7edf871048217e47b8562c2ac897039
Markus Staab <maggus.staab@googlemail.com>
2023-12-19 15:50:01 +0100

    fix

---
+++ tests/SingleTimeTagFetcher.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace staabm\PHPStanTodoBy\Tests;
+
+use staabm\PHPStanTodoBy\TagFetcher;
+
+final class SingleTimeTagFetcher implements TagFetcher
+{
+    static private int $counter = 0;
+    public function fetchLatestTagVersion(?string $workingDirectory): string
+    {
+        if (self::$counter === 0) {
+            self::$counter++;
+
+            return '1.2.3';
+        }
+
+        throw new \RuntimeException('Could not determine latest git tag');
+    }
+}
