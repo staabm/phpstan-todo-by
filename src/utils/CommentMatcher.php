e7cb0e33c587886263ad344786e7b7a7e9c60707
Markus Staab <maggus.staab@googlemail.com>
2023-12-20 09:30:18 +0100

    Refactor common logic into CommentMatcher

---
+++ src/utils/CommentMatcher.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace staabm\PHPStanTodoBy\utils;
+
+use PhpParser\Comment;
+use PHPStan\Node\VirtualNode;
+use PhpParser\Node;
+
+final class CommentMatcher {
+    /**
+     * @return iterable<Comment, array<mixed>>
+     */
+    static public function matchComments(Node $node, string $pattern): iterable {
+        if (
+            $node instanceof VirtualNode
+            || $node instanceof Node\Expr
+        ) {
+            // prevent duplicate errors
+            return [];
+        }
+
+        foreach ($node->getComments() as $comment) {
+
+            $text = $comment->getText();
+
+            /**
+             * PHP doc comments have the entire multi-line comment as the text.
+             * Since this could potentially contain multiple "todo" comments, we need to check all lines.
+             * This works for single line comments as well.
+             *
+             * PREG_OFFSET_CAPTURE: Track where each "todo" comment starts within the whole comment text.
+             * PREG_SET_ORDER: Make each value of $matches be structured the same as if from preg_match().
+             */
+            if (
+                preg_match_all($pattern, $text, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER) === false
+                || count($matches) === 0
+            ) {
+                continue;
+            }
+
+            yield $comment => $matches;
+        }
+    }
+}
