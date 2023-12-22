1186e9307c4c2f054fe3ea7405d7612f881665fd
Markus Staab <maggus.staab@googlemail.com>
2023-12-22 15:20:05 +0100

    Implement TodoByConfigVersionRule

---
+++ tests/data/versionToggles.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace ConfigToggles;
+
+// TODO: minimumApiClientVersion:2.0 This can be removed once all API consumers use client version 2.0 or higher
+
+// TODO: unknownToggle:2.0 not configured
