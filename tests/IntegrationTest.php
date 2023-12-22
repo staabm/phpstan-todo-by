5dd7c893fd32ece39868237f9df7e4d358ac8f3e
Markus Staab <maggus.staab@googlemail.com>
2023-12-22 16:18:42 +0100

    fix build

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -13,14 +13,13 @@ use PHPStan\Testing\PHPStanTestCase;
 final class IntegrationTest extends PHPStanTestCase
 {
     // test all rules at once to make sure rule errors do not overlap
-    public function testE2E()
+    public function testE2E(): void
     {
         $errors = $this->runAnalyse(__DIR__ . '/data/e2e.php');
         static::assertCount(2, $errors);
 
         $this->assertSame('Expired on 2023-12-14: fix it.', $errors[0]->getMessage());
         $this->assertSame('"php" version requirement ">=7" satisfied.', $errors[1]->getMessage());
-
     }
 
     public static function getAdditionalConfigFiles(): array
@@ -41,6 +40,7 @@ final class IntegrationTest extends PHPStanTestCase
         $analyser = self::getContainer()->getByType(Analyser::class);
         /** @var FileHelper $fileHelper */
         $fileHelper = self::getContainer()->getByType(FileHelper::class);
+        /** @phpstan-ignore-next-line missing bc promise */
         $errors = $analyser->analyse([$file], null, null, true, $allAnalysedFiles)->getErrors();
         foreach ($errors as $error) {
             $this->assertSame($fileHelper->normalizePath($file), $error->getFilePath());
