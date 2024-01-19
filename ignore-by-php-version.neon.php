c4faae671fcd10434aa380e81bc876ffadcafde8
Markus Staab <maggus.staab@googlemail.com>
2024-01-19 13:16:06 +0100

    per php-version baseline

---
+++ ignore-by-php-version.neon.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+$includes = [];
+if (PHP_VERSION_ID < 80000) {
+	$includes[] = __DIR__ . '/baseline-7.4.neon';
+}
+
+$config = [];
+$config['includes'] = $includes;
+$config['parameters']['phpVersion'] = PHP_VERSION_ID;
+
+return $config;
