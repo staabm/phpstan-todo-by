3c2b7f755965f5bb58354e7c492d3cb8de988f8e
Markus Staab <maggus.staab@googlemail.com>
2025-12-04 07:51:25 +0100

    Fix curl-share handling on PHP < 8.5 (#173)

--- src/utils/HttpClient.php
+++ src/utils/HttpClient.php
@@ -2,12 +2,20 @@
 
 namespace staabm\PHPStanTodoBy\utils;
 
+use CurlShareHandle;
+use CurlSharePersistentHandle;
 use RuntimeException;
 
+use function function_exists;
 use function is_string;
 
 final class HttpClient
 {
+    /**
+     * @var CurlShareHandle|CurlSharePersistentHandle|resource|null
+     */
+    private $shareHandle;
+
     /**
      * @param non-empty-array<non-empty-string> $urls
      * @param list<string> $headers
@@ -16,15 +24,19 @@ final class HttpClient
      */
     public function getMulti(array $urls, array $headers): array
     {
-        if (function_exists('curl_share_init_persistent')) {
-            $share = curl_share_init_persistent([
-                CURL_LOCK_DATA_DNS,
-                CURL_LOCK_DATA_CONNECT,
-            ]);
-        } else {
-            $share = curl_share_init();
-            curl_share_setopt($share, CURLSHOPT_SHARE, CURL_LOCK_DATA_DNS);
-            curl_share_setopt($share, CURLSHOPT_SHARE, CURL_LOCK_DATA_CONNECT);
+        if (null === $this->shareHandle) {
+            if (function_exists('curl_share_init_persistent')) {
+                $this->shareHandle = curl_share_init_persistent([
+                    CURL_LOCK_DATA_DNS,
+                    CURL_LOCK_DATA_CONNECT,
+                    CURL_LOCK_DATA_SSL_SESSION,
+                ]);
+            } else {
+                $this->shareHandle = curl_share_init();
+                curl_share_setopt($this->shareHandle, CURLSHOPT_SHARE, CURL_LOCK_DATA_DNS);
+                curl_share_setopt($this->shareHandle, CURLSHOPT_SHARE, CURL_LOCK_DATA_CONNECT);
+                curl_share_setopt($this->shareHandle, CURLSHOPT_SHARE, CURL_LOCK_DATA_SSL_SESSION);
+            }
         }
 
         $mh = curl_multi_init();
@@ -37,7 +49,7 @@ final class HttpClient
                 throw new RuntimeException('Could not initialize cURL connection');
             }
 
-            curl_setopt($curl, CURLOPT_SHARE, $share);
+            curl_setopt($curl, CURLOPT_SHARE, $this->shareHandle);
 
             // see https://stackoverflow.com/a/27776164/1597388
             curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 3);
