cfdeb254c269006c5774430d3dda4de849916100
Martin Lutonsky <martin.lutonsky@gmail.com>
2025-12-05 17:38:27 +0100

    Add failing unit test for TodoByVersionRule for "testing" tag

---
+++ tests/TodoByVersionRuleTestingTagFetchterTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace staabm\PHPStanTodoBy\Tests;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use staabm\PHPStanTodoBy\TodoByVersionRule;
+use staabm\PHPStanTodoBy\utils\ExpiredCommentErrorBuilder;
+use staabm\PHPStanTodoBy\utils\ReferenceVersionFinder;
+
+/**
+ * @extends RuleTestCase<TodoByVersionRule>
+ * @internal
+ */
+final class TodoByVersionRuleTestingTagFetchterTest extends RuleTestCase
+{
+    private string $referenceVersion;
+
+    protected function getRule(): Rule
+    {
+        return new TodoByVersionRule(
+            true,
+            new ReferenceVersionFinder($this->referenceVersion, new StaticTagFetcher('testing')),
+            new ExpiredCommentErrorBuilder(true)
+        );
+    }
+
+    public function testNoTagIsFetched(): void
+    {
+        $this->referenceVersion = 'nextMajor';
+
+        $this->analyse([__DIR__ . '/data/tagNotFound.php'], []);
+    }
+}
